363ce22622dcb7294a4489500c597a17d13060af
Christoph Decker <C.Decker@metalldeutsch.com>
2024-05-17 09:45:37 +0200

    change directories

---
+++ src/chd.CaraVan.Web/Program.cs
@@ -0,0 +1,24 @@
+using chd.CaraVan.UI.Components;
+using chd.CaraVan.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode().
+    AddAdditionalAssemblies(typeof(Routes).Assembly);
+
+app.Run();
