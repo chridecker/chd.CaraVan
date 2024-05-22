651152276cd15213230cadd53f1a9a392b6a464a
Christoph Decker <C.Decker@metalldeutsch.com>
2024-05-22 11:07:14 +0200

    Reload

--- src/chd.CaraVan.UI/Components/Pages/Cards/DeviceCard.razor.cs
+++ src/chd.CaraVan.UI/Components/Pages/Cards/DeviceCard.razor.cs
@@ -5,31 +5,45 @@ using Microsoft.AspNetCore.Components;
 
 namespace chd.CaraVan.UI.Components.Pages.Cards
 {
-    public partial class DeviceCard
+    public partial class DeviceCard : IDisposable
     {
         [Parameter] public DeviceDto DeviceDto { get; set; }
 
         [Inject] private NavigationManager _navigationManager { get; set; }
         [Inject] private IDataService _dataService { get; set; }
 
-
+        private CancellationTokenSource _cts = new();
         private (decimal?, DateTime?) _temperature;
         private (decimal?, DateTime?) _humidity;
+        private Task _reload;
 
         private string _data(decimal? data) => !data.HasValue ? "NaN" : data.Value.ToString("n2");
 
         protected override async Task OnInitializedAsync()
         {
-            var temp = await this._dataService.GetLatestDataToDeviceAsync(this.DeviceDto.Id, EDataType.Temperature);
-            var humidity = await this._dataService.GetLatestDataToDeviceAsync(this.DeviceDto.Id, EDataType.Humidity);
-            this._temperature = (temp?.Value, temp?.RecordDateTime);
-            this._humidity = (humidity?.Value, humidity?.RecordDateTime);
+            this._reload = this.ReloadData(this._cts.Token);
+
             await base.OnInitializedAsync();
         }
-
-        private void NavigateToDevice()
+        private Task ReloadData(CancellationToken cancellationToken) => Task.Run(async () =>
         {
-            this._navigationManager.NavigateTo($"/device/{this.DeviceDto.Id}");
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var temp = await this._dataService.GetLatestDataToDeviceAsync(this.DeviceDto.Id, EDataType.Temperature, cancellationToken);
+                var humidity = await this._dataService.GetLatestDataToDeviceAsync(this.DeviceDto.Id, EDataType.Humidity, cancellationToken);
+                this._temperature = (temp?.Value, temp?.RecordDateTime);
+                this._humidity = (humidity?.Value, humidity?.RecordDateTime);
+                await this.InvokeAsync(this.StateHasChanged);
+                await Task.Delay(TimeSpan.FromMinutes(1), cancellationToken);
+
+            }
+        }, cancellationToken);
+
+        private void NavigateToDevice() => this._navigationManager.NavigateTo($"/device/{this.DeviceDto.Id}");
+
+        public void Dispose()
+        {
+            this._cts.Cancel();
         }
     }
 }
