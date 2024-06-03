14655f2e450186400c79c4fdb8f34b9024dede9c
chridecker <46068791+chridecker@users.noreply.github.com>
2024-06-03 06:41:02 +0200

    Update InfluxContext.cs

--- src/chd.CaraVan.DataAccess/InfluxContext.cs
+++ src/chd.CaraVan.DataAccess/InfluxContext.cs
@@ -1,5 +1,5 @@
-﻿using InfluxDB.Client;
-using InfluxDB.Client.Writes;
+using InfluxDB3.Client;
+using InfluxDB3.Client.Write;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
@@ -10,67 +10,84 @@ using System.Threading.Tasks;
 namespace chd.CaraVan.DataAccess
 {
     public class InfluxContext : IInfluxContext
+{
+    private const string MES_BATTERY = "battery";
+    private const string MES_SOLAR = "solar";
+    private const string MES_SENSOR = "sensor";
+
+    private const string FIELD_AMPERE = "ampere";
+    private const string FIELD_AMPEREH = "ampereH";
+    private const string FIELD_PERCENT = "percent";
+    private const string FIELD_VOLTAGE = "voltage";
+    private const string FIELD_TEMPERATURE = "temperature";
+    private const string FIELD_STATE = "state";
+    private const string FIELD_TIME = "time";
+
+    private const string TAG_ID = "temperature";
+
+    private readonly InfluxDBClient _client;
+    private readonly IOptionsMonitor<InfluxSettings> _optionsMonitor;
+
+    public InfluxContext(IOptionsMonitor<InfluxSettings> optionsMonitor)
     {
-        private readonly InfluxDBClient _client;
-        private readonly IOptionsMonitor<InfluxSettings> _optionsMonitor;
-
-        public InfluxContext(IOptionsMonitor<InfluxSettings> optionsMonitor)
-        {
-            this._optionsMonitor = optionsMonitor;
-
-            this._client = new InfluxDBClient(new InfluxDBClientOptions(this._optionsMonitor.CurrentValue.Url)
-            {
-                Token = this._optionsMonitor.CurrentValue.ApiKey,
-                Org = this._optionsMonitor.CurrentValue.Org,
-                Bucket = this._optionsMonitor.CurrentValue.Database,
-            });
-        }
-
-        public async Task WriteBatteryData(DateTime time, decimal ampere, decimal ampereH, decimal percent, decimal voltage)
-        {
-            var point = PointData.Measurement("battery")
-                .Timestamp(time, InfluxDB.Client.Api.Domain.WritePrecision.S)
-                .Field("ampere", ampere)
-                .Field("ampereH", ampereH)
-                .Field("percent", percent)
-                .Field("voltage", voltage);
-            await this._client.GetWriteApiAsync().WritePointAsync(point, InfluxConstants.DATABASE);
-        }
-        public async Task WriteSolarData(DateTime time, decimal ampere, byte state)
-        {
-            var point = PointData.Measurement("solar")
-                .Timestamp(time, InfluxDB.Client.Api.Domain.WritePrecision.S)
-                .Field("ampere", ampere)
-                .Field("state", state);
-            await this._client.GetWriteApiAsync().WritePointAsync(point, InfluxConstants.DATABASE);
-        }
-
-        public async Task WriteSensorData(DateTime time, int id, decimal temp)
-        {
-            var point = PointData.Measurement("sensor")
-                .Timestamp(time, InfluxDB.Client.Api.Domain.WritePrecision.S)
-                .Tag("id", id.ToString())
-                .Field("temperature", temp);
-            try
-            {
-                await this._client.GetWriteApiAsync().WritePointAsync(point, InfluxConstants.DATABASE);
-            }
-            catch (Exception ex)
-            {
-            }
-        }
-
-
-
-        public void Dispose()
-        {
-            this._client.Dispose();
-        }
+        this._client = new InfluxDBClient(optionsMonitor.CurrentValue.Url, optionsMonitor.CurrentValue.ApiKey);
+        this._optionsMonitor = optionsMonitor;
     }
-    public interface IInfluxContext : IDisposable
+
+    public async Task WriteBatteryData(DateTime time, decimal ampere, decimal ampereH, decimal percent, decimal voltage, CancellationToken cancellationToken)
     {
-        Task WriteBatteryData(DateTime time, decimal ampere, decimal ampereH, decimal percent, decimal voltage);
-        Task WriteSensorData(DateTime time, int id, decimal temp);
-        Task WriteSolarData(DateTime time, decimal ampere, byte state);
+        var point = PointData.Measurement(MES_BATTERY)
+            .SetTimestamp(time)
+            .SetField(FIELD_AMPERE, ampere)
+            .SetField(FIELD_AMPEREH, ampereH)
+            .SetField(FIELD_PERCENT, percent)
+            .SetField(FIELD_VOLTAGE, voltage);
+        await this._client.WritePointAsync(point, database: this._optionsMonitor.CurrentValue.Database, cancellationToken: cancellationToken);
     }
+    public async Task WriteSolarData(DateTime time, decimal ampere, byte state, CancellationToken cancellationToken)
+    {
+        var point = PointData.Measurement(MES_SOLAR)
+            .SetTimestamp(time)
+            .SetField(FIELD_AMPERE, ampere)
+            .SetField(FIELD_STATE, state);
+        await this._client.WritePointAsync(point, database: this._optionsMonitor.CurrentValue.Database, cancellationToken: cancellationToken);
+    }
+
+    public async Task WriteSensorData(DateTime time, int id, decimal temp, CancellationToken cancellationToken)
+    {
+        var point = PointData.Measurement(MES_SENSOR)
+            .SetTimestamp(time)
+            .SetTag(TAG_ID, id.ToString())
+            .SetField(FIELD_TEMPERATURE, temp);
+        await this._client.WritePointAsync(point, database: this._optionsMonitor.CurrentValue.Database, cancellationToken: cancellationToken);
+    }
+    //public async Task GetSolarData(CancellationToken cancellationToken)
+    //{
+    //    var sql = @$"SELECT * FROM {MES_SOLAR}
+    //                    WHERE time >= now() - interval '1 hour'";
+    //    try
+    //    {
+    //        await foreach (var res in this._client.Query(sql, database: this._optionsMonitor.CurrentValue.Database))
+    //        {
+    //        }
+    //    }
+    //    catch (Exception ex)
+    //    {
+
+    //    }
+    //}
+
+
+    public void Dispose()
+    {
+        this._client.Dispose();
+    }
+}
+public interface IInfluxContext : IDisposable
+{
+    //Task GetSolarData(CancellationToken cancellationToken);
+    Task WriteBatteryData(DateTime time, decimal ampere, decimal ampereH, decimal percent, decimal voltage, CancellationToken cancellationToken);
+    Task WriteSensorData(DateTime time, int id, decimal temp, CancellationToken cancellationToken);
+    Task WriteSolarData(DateTime time, decimal ampere, byte state, CancellationToken cancellationToken);
+}
 }
