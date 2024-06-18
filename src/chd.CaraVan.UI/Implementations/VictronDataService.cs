4824fbe0419cff380d5ec5a203f1e932bb555b8f
chridecker <46068791+chridecker@users.noreply.github.com>
2024-06-18 07:22:23 +0200

    Update VictronDataService.cs

--- src/chd.CaraVan.UI/Implementations/VictronDataService.cs
+++ src/chd.CaraVan.UI/Implementations/VictronDataService.cs
@@ -10,12 +10,12 @@ namespace chd.CaraVan.UI.Implementations
     public class VictronDataService : IVictronDataService
     {
         private VictronData _data;
-        public void Add(VictronData data) => this._data = data;
+        public Task Add(VictronData data, CancellationToken cancellationToken = default) => Task.Run(()=>{this._data = data;}, cancellationToken);
         public Task<VictronData> GetData(CancellationToken cancellationToken) => Task.FromResult(this._data);
     }
     public interface IVictronDataService
     {
-        void Add(VictronData data);
+        Task Add(VictronData data, CancellationToken cancellationToken = default);
         Task<VictronData> GetData(CancellationToken cancellationToken = default);
     }
 }
