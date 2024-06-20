0126d3bea25ab40e9ec58f4089ea8a804a8bef0e
chridecker <46068791+chridecker@users.noreply.github.com>
2024-06-20 10:30:18 +0200

    Update DIExtensions.cs

--- src/chd.CaraVan.UI/Extensions/DIExtensions.cs
+++ src/chd.CaraVan.UI/Extensions/DIExtensions.cs
@@ -27,6 +27,7 @@ namespace chd.CaraVan.UI.Extensions
             services.AddSingleton<ISettingService, SettingService>();
             services.AddSingleton<IAESManager, AESManager>();
             services.AddSingleton<IPiManager, PiManager>();
+            services.AddSingleton<IEmailService, EmailService>();
             services.AddSingleton<ISystemManager, SystemManager>();
             services.AddSingleton<IVictronDataService, VictronDataService>();
             services.AddSingleton<IRuuviTagDataService, RuuviTagDataService>();
